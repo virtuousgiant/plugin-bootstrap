91dab2971b592b98250890538edae0f1a4a87de3
Nathan Hangen <nhangen@gmail.com>
2013-05-10 11:02:26 -0400

    basic menu template with metabox styling and sidebar.

---
+++ templates/admin/pbSettings.php
@@ -0,0 +1,68 @@
+<div class="wrap">
+	<div class="icon32" id="icon-options-general"></div><h2>Headline</h2>
+	<div class="postbox-container" style="width:65%; margin-right: 5%">
+		<div class="metabox-holder">
+			<div class="meta-box-sortables" style="min-height:0;">
+				<div class="postbox">
+					<h3 class="hndle"><span>Settings</span></h3>
+					<div class="inside">
+						<form method="POST" action="" id="idmsg-settings" name="idmsg-settings">
+							<div class="form-input">
+								<input type="text" name="text-input" id="text-input"/>
+								<label for="text-input">Just a simple text input to get us rolling</label>
+							</div>
+							<div class="form-check">
+								<input type="checkbox" name="checkbox-input" id="checkbox-input"/>
+								<label for="checkbox-input">Just a simple checkbox for more rolling</label>
+							</div>
+							<div class="submit">
+								<button name="submit" id="submit" class="button button-primary">Submit</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Begin Sidebar -->
+	<div class="postbox-container" style="width:20%;">
+		<div class="metabox-holder">
+			<div class="meta-box-sortables" style="min-height:0;">
+				<div class="postbox">
+					<h3 class="hndle"><span>About This Plugin</span></h3>
+					<div class="inside">
+						<p>Plugin Bootstrap is designed and developed by <a href="http://virtuousgiant.com">Virtuous Giant</a>.</p>
+						<p>Get in touch with us on <a href="https://www.facebook.com/virtuousgiant">Facebook</a>, Twitter <a href="http://twitter.com/virtuousgiant">@virtuousgiant</a>, or App.net <a href="http://alpha.app.net/vg">@VG</a>.</p>
+					</div>
+				</div>
+			</div>
+			<div class="meta-box-sortables" style="min-height:0;">
+				<div class="postbox">
+					<h3 class="hndle"><span>Support this Plugin</span></h3>
+					<div class="inside">
+						<div style="text-align: center;">
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+								<input type="hidden" name="cmd" value="_s-xclick">
+								<input type="hidden" name="hosted_button_id" value="AYQCM6XAX5BTL">
+								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="meta-box-sortables" style="min-height:0;">
+				<div class="postbox">
+					<h3 class="hndle"><span>Links</span></h3>
+					<div class="inside">
+						<ul>
+							<li>IgnitionDeck - <a href="http://ignitiondeck.com">Crowdfunding for Wordpress</a></li>
+							<li>Virtuous Giant - <a href="http://VirtuousGiant.com">Virtuous Giant</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Sidebar -->
+</div>
